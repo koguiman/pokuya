15315e2f8a5721f8580f86e49f488f871d639965
koguiman <koguiman@gmail.com>
2014-09-16 11:02:57 +0200

    solving problems in the use of functions

---
+++ Pokuya_teststools.py
@@ -0,0 +1,96 @@
+###This script is used to generate the pokuya software
+###pokuya is an open source software developed to obtain 
+###the same basic functionality as a power quiality analyzer
+
+import pylab
+import numpy, scipy
+import matplotlib.pyplot as plt 
+from math import *
+from cmath import *
+from scipy import linalg as nn
+import numpy as np
+import csv
+from scipy import signal as sg
+from csvtonum import *
+from pokuyatools import *
+
+##########Single phase tests
+###########inputs part
+name='Test3.csv' 
+fe=float(50) #electrical frequecy. Eur=50Hz, American countries=60Hz
+Ncycle=3 #number of cycles to be analyzed
+chnncurr=1
+###########output ports
+reportname='{0}{1}'.format('reportof_',name)
+########################
+##open the file with data
+x=csv.reader(open(name),delimiter=',')
+###load data and info from source file
+[data,info]=csvtodato(x)
+
+#create the object X for analysis
+X={'data':data,'chancur':chnncurr,'Ncycles':Ncycle,'fe':fe}
+############################
+report=sphan(X)
+
+##################################################################
+###Report file generation
+savereport(report,reportname)
+
+#################################################################
+###########Ploting part
+
+####time domain signals
+plt.figure(1)
+plt.subplot(311)
+plt.plot(time[0:Ln],I)
+plt.title('Current [A]')
+plt.ylabel('I(t) [A]')
+plt.grid(True)
+plt.subplot(312)
+plt.plot(time[0:Ln],V)
+plt.title('Voltage [V]')
+plt.ylabel('V(t) [V]')
+plt.grid(True)
+#plt.xlabel('Time [s]')
+plt.subplot(313)
+plt.plot(time[0:Ln],pinst)
+plt.title('Instantaneous power [W]')
+plt.ylabel('P(t) [W]')
+plt.xlabel('Time [s]')
+plt.grid(True)
+
+###Fourier spectrum
+lim=int(Freq.shape[0]/2)
+plt.figure(2)
+plt.subplot(211)
+#plt.plot(Freq[0:lim],F_Id[0:lim])
+plt.bar(np.hstack([array([0.0]),Fhs]),np.hstack([array([I0]),Ihs]), width=0.84, label="Current",align="center")
+plt.xlim([0,50*50])
+plt.ylabel('I($\omega$) [A$_{rms}$]')
+plt.grid(True)
+plt.subplot(212)
+#plt.plot(Freq[0:lim],F_Vd[0:lim])
+plt.bar(np.hstack([array([0.0]),Fhs]),np.hstack([array([V0]),Vhs]), width=0.84, label="Current",align="center")
+plt.xlim([0,50*50])
+plt.ylabel('V($\omega$) [V$_{rms}$]')
+plt.grid(True)
+###Fourier spectrum in % of the I50
+plt.figure(3)
+plt.subplot(211)
+#plt.plot(Freq[0:lim],F_Id[0:lim])
+plt.bar(np.hstack([array([0.0]),Fhs]),100*np.hstack([array([I0]),Ihs])/I50, width=0.84, label="Current",align="center")
+plt.xlim([0,50*50])
+plt.ylim([0,101])
+plt.ylabel('I($\omega$) [%]')
+plt.grid(True)
+plt.subplot(212)
+#plt.plot(Freq[0:lim],F_Vd[0:lim])
+plt.bar(np.hstack([array([0.0]),Fhs]),100*np.hstack([array([V0]),Vhs])/V50, width=0.84, label="Current",align="center")
+plt.xlim([0,50*50])
+plt.ylim([0,101])
+plt.ylabel('V($\omega$) [ %]')
+plt.grid(True)
+plt.xlabel('Frequency [Hz]')
+
+plt.show()
